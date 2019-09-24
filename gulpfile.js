388363d6db3b8ef6bf30cea853bb539e12434ef6
Gavin Austin <2gaustin@gmail.com>
2019-09-23 21:36:41 -0600

    Add function to compile SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -7,3 +7,22 @@ const minifyCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const changed = require('gulp-changed');
+
+////////////////
+// - SCSS / CSS
+////////////////
+
+const SCSS_SRC = './src/assets/scss/**/*.scss';
+const SCSS_DEST = './src/assets/css';
+
+//Compile SCSS
+function style() {
+    return gulp.src(SCSS_SRC)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(minifyCSS())
+        .pipe(rename({ suffix: '.min'}))
+        .pipe(changed(SCSS_DEST))
+        .pipe(gulp.dest(SCSS_DEST));
+}
+
+exports.default = style;
