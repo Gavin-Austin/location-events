8d0c739641d0b755d57dde1dfd369fe776945e43
Gavin Austin <2gaustin@gmail.com>
2019-09-24 11:09:40 -0600

    Create footer.js, homePage.js

---
+++ src/components/pages/homePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Homepage() {
+    return (
+        <div className={"container-fluid"}>
+            Home Page Content Here
+        </div>
+    )
+}
+
+export default Homepage;
