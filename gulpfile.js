1c50bd8ff981085f11943d203a25c26c917fc8fb
Gavin Austin <2gaustin@gmail.com>
2019-09-23 20:44:26 -0600

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+//Dependencies
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const minifyCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const changed = require('gulp-changed');
