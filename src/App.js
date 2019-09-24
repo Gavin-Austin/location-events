d0d0fd5b2efaf54848aac3991cb9d31b7828b7b6
Gavin Austin <2gaustin@gmail.com>
2019-09-24 16:19:39 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';
 
 //Components
 import Header from './components/headerComponent/header';
 import Footer from './components/footerComponent/footer';
 import Homepage from './components/pages/homePage';
+import Popular from './components/pages/popular';
 
 //Includes
 import './assets/css/default.min.css';
 
 function App() {
-  return (
-    <div className="App">
-        <Header/>
-        <Homepage/>
-        <Footer/>
-    </div>
-  )
+    return (
+        <Router>
+            <div className="App">
+                <Header/>
+                <Route exact path={'/'} component={Homepage} />
+                <Route exact path={'/Popular'} component={Popular} />
+                <Footer/>
+            </div>
+        </Router>
+    )
 }
 
 export default App;
