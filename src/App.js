b45851356c99c773a6f40ee5ed1100f2f49217c7
Gavin Austin <2gaustin@gmail.com>
2019-09-24 10:50:13 -0600

    Update App.js, default.scss, default.min.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+//Components
+import Header from './components/headerComponent/header';
+
+//Includes
+import './assets/css/default.min.css';
+
 function App() {
   return (
     <div className="App">
-      Test
+        <Header/>
     </div>
   )
 }
