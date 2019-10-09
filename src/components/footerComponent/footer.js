e56b819f7f243a61355e0d7fdc587f430d664793
Gavin Austin <2gaustin@gmail.com>
2019-10-08 19:30:36 -0600

    Add repo link to footer.js

--- src/components/footerComponent/footer.js
+++ src/components/footerComponent/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <footer>
-            Test Footer
+            The repository can be found at <a href="https://github.com/Gavin-Austin/location-events" target="_blank">https://github.com/Gavin-Austin/location-events</a>
         </footer>
     )
 }
