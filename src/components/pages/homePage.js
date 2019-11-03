93e06d1d41ea154ce72db97bdae821a210427918
Gavin Austin <2gaustin@gmail.com>
2019-11-03 10:50:31 -0700

    Fix build error

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class Homepage extends React.Component {
+class Homepage extends Component {
     constructor(props) {
         super(props);
         this.state = {zip: ''};
