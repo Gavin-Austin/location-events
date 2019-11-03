47469222d42889fe59e7794f681761c084edca10
Gavin Austin <2gaustin@gmail.com>
2019-11-03 10:42:10 -0700

    Add user input form

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Homepage() {
-    return (
-        <div className={"container-fluid"}>
-            <h1>
-                Welcome to Location Events
-            </h1>
-            <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-        </div>
-    )
+class Homepage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {zip: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({zip: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('A zip code was submitted: ' + this.state.zip);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="container-fluid">
+                <div>
+                <h1> Welcome to Location Events </h1>
+                <h2> Enter Your Zip Code Below: </h2>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        <input type="text" zip={this.state.zip} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" zip="Submit" />
+                </form>
+            </div>
+            </div>
+        );
+    }
 }
 
 export default Homepage;
